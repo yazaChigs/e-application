f9e52069e6ce94fe86c03c2b370a4e67c75a319d
yazaChigs <eleazarmachigs@gmail.com>
2019-09-05 21:18:35 +0200

    yaza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'e-forms-fr';
+export class AppComponent implements OnInit {
+  public form: FormGroup;
+  public contactList: FormArray;
+  list: any[];
+  selected : any;
+  questionType = ['text', 'check box', 'select', 'number', 'radio'];
+
+  // returns all form groups under contacts
+  get contactFormGroup() {
+    return this.form.get('contacts') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: [null, Validators.compose([Validators.required])],
+      organization: [null],
+      contacts: this.fb.array([this.createContact()])
+    });
+
+    // set contactlist to this field
+    this.contactList = this.form.get('contacts') as FormArray;
+  }
+
+  // contact formgroup
+  createContact(): FormGroup {
+    return this.fb.group({
+      // type: ['email', Validators.compose([Validators.required])], // i.e Email, Phone
+      // name: [null, Validators.compose([Validators.required])], // i.e. Home, Office
+      // value: [null, Validators.compose([Validators.required, Validators.email])]
+      type: new FormControl(),
+      question: new FormControl(),
+      weight: new FormControl(),
+      required: new FormControl(),
+      answer: new FormControl()
+    });
+  }
+
+  // add a contact form group
+  addContact() {
+    this.contactList.push(this.createContact());
+    this.list = this.form.get('contacts').value;
+  }
+
+  // remove contact from group
+  removeContact(index) {
+    // this.contactList = this.form.get('contacts') as FormArray;
+    this.contactList.removeAt(index);
+  }
+
+  // triggered to change validation of value field type
+  changedFieldType() {
+
+    // if (this.getContactsFormGroup(index).controls['type'].value === 'email') {
+    //   validators = Validators.compose([Validators.required, Validators.email]);
+    // } else {
+    //   validators = Validators.compose([
+    //     Validators.required,
+    //     Validators.pattern(new RegExp('^\\+[0-9]?()[0-9](\\d[0-9]{9})$')) // pattern for validating international phone number
+    //   ]);
+    }
+
+  //   this.getContactsFormGroup(index).controls['value'].setValidators(
+  //     validators
+  //   );
+
+  //   this.getContactsFormGroup(index).controls['value'].updateValueAndValidity();
+  // }
+
+  // get the formgroup under contacts form array
+  getContactsFormGroup(index): FormGroup {
+    // this.contactList = this.form.get('contacts') as FormArray;
+    const formGroup = this.contactList.controls[index] as FormGroup;
+    return formGroup;
+  }
+
+  // method triggered when form is submitted
+  submit() {
+    const family = this.list,
+    unique = [...Array.from(new Set(family.map(a => a.name)))];
+
+console.log(unique);
+    console.log(this.form.value);
+  }
+
+  checkDuplicateInObject(propertyName, inputArray) {
+    var seenDuplicate = false,
+        testObject = {};
+
+    inputArray.map(function(item) {
+      var itemPropertyName = item[propertyName];
+      if (itemPropertyName in testObject) {
+        testObject[itemPropertyName].duplicate = true;
+        item.duplicate = true;
+        seenDuplicate = true;
+      }
+      else {
+        testObject[itemPropertyName] = item;
+        delete item.duplicate;
+      }
+    });
+
+    console.log(seenDuplicate);
+    return seenDuplicate;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,89 @@
-import { BrowserModule } from '@angular/platform-browser';
+// import { Global } from './global';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+// import { LoginModule } from './login/login.module';
+// import { LayoutMainModule } from './layout/layout.module';
+import 'hammerjs';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { Interceptor } from './core/inteceptor';
+import { SnotifyService, ToastDefaults, SnotifyModule } from 'ng-snotify';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule,
+  MatCardModule, MatMenuModule, MatDividerModule, MatLabel, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatCheckboxModule, MatRadioModule} from '@angular/material';
+// import { AuthService } from './core/auth.service';
+// import { TokenStorage } from './core/token.storage';
+// import { Messages } from './util/messages';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+// import { reducer } from './layout/patient/store/reducers/patient.reducer';
+// import { reducers, metaReducers } from './store/reducers';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+// import { SanitizeHtmlPipe } from './util/sanitize-html-pipe';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent,
+    //  SanitizeHtmlPipe
+    ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    CommonModule,
+    AppRoutingModule,
+    MatButtonModule,
+    ReactiveFormsModule, FormsModule,
+    // LoginModule,
+    // LoadingBarHttpClientModule,
+    // LoadingBarRouterModule,
+    // LayoutMainModule,
+    FlexLayoutModule, MatButtonModule, MatSelectModule, MatOptionModule,
+    MatToolbarModule, MatFormFieldModule, MatInputModule, MatCheckboxModule,
+    MatSidenavModule, MatRadioModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatDividerModule,
+    MatMenuModule,
+    NoopAnimationsModule,
+    SnotifyModule,
+    // NgxPermissionsModule.forRoot(),
+    // StoreModule.forRoot(reducers, { metaReducers }),
+    // EffectsModule.forRoot([]),
+    // environment.production
+    //   ? []
+    //   : StoreDevtoolsModule.instrument({
+    //     name: 'MHR'
+    //   }),
   ],
-  providers: [],
+
+  entryComponents: [],
+  providers: [
+    // AuthService,
+    // TokenStorage,
+    // Messages,
+    // Global,
+    // { provide: LocationStrategy, useClass: HashLocationStrategy },
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: Interceptor,
+    //   multi: true
+    // },
+    // SnotifyService,
+    // {
+    //   provide: 'SnotifyToastConfig',
+    //   useValue: ToastDefaults
+    // }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
